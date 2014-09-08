ee821782b02899d5457bca6e8c11011f902bc631
Wardell James <wjames@student.neumont.edu>
2014-09-08 15:48:41 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/the_epic_game_of_randomness/trunk@83 8e5f334d-e8ea-6740-b88d-dc217f5a1e1b

--- src/Enemys/Hobo.js
+++ src/Enemys/Hobo.js
@@ -2,7 +2,7 @@
 Hobo = function() {
 	this.initialize();
 }
-Hobo._SpriteSheet = new createjs.SpriteSheet({images: ["Hobo.png"], frames: [[0,0,69,93,0,23.9,20.1],[69,0,92,107,0,38.9,19.1],[161,0,76,100,0,27.9,22.1]]});
+Hobo._SpriteSheet = new createjs.SpriteSheet({images: ["hobo.png"], frames: [[0,0,200,300,0,79.65,297.85],[0,300,200,300,0,92.65,271.85],[0,600,200,300,0,68.65,288.85]]});
 var Hobo_p = Hobo.prototype = new createjs.Sprite();
 Hobo_p.Sprite_initialize = Hobo_p.initialize;
 Hobo_p.initialize = function() {
