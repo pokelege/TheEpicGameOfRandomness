eaf43d317345a72f997af4e49de57d6256a0b7c1
Wardell James <wjames@student.neumont.edu>
2014-09-10 03:50:31 +0000

    Specials are done
    git-svn-id: https://svn.neumont.edu:8443/svn/the_epic_game_of_randomness/trunk@118 8e5f334d-e8ea-6740-b88d-dc217f5a1e1b

---
+++ src/Enemys/New stuff/Specials/Heave Special.js
@@ -0,0 +1,14 @@
+﻿(function(window) {
+Koolaid_Jug = function() {
+	this.initialize();
+}
+Koolaid_Jug._SpriteSheet = new createjs.SpriteSheet({images: ["Heave Special.png"], frames: [[0,0,112,114,0,49.95,96.05],[112,0,137,139,0,61.95,108.05],[249,0,154,155,0,70.95,116.05],[0,155,159,159,0,72.95,118.05],[159,155,155,154,0,70.95,116.05],[314,155,139,137,0,62.95,107.05],[0,314,114,112,0,49.95,95.05],[114,314,139,137,0,62.95,107.05],[253,314,155,154,0,70.95,116.05],[0,468,160,160,0,72.95,119.05],[160,468,233,185,0,93.95,88.05]]});
+var Koolaid_Jug_p = Koolaid_Jug.prototype = new createjs.Sprite();
+Koolaid_Jug_p.Sprite_initialize = Koolaid_Jug_p.initialize;
+Koolaid_Jug_p.initialize = function() {
+	this.Sprite_initialize(Koolaid_Jug._SpriteSheet);
+	this.paused = false;
+}
+window.Koolaid_Jug = Koolaid_Jug;
+}(window));
+
---
+++ src/Enemys/New stuff/Specials/Kamie Special.js
@@ -0,0 +1,14 @@
+﻿(function(window) {
+Sound_Wave = function() {
+	this.initialize();
+}
+Sound_Wave._SpriteSheet = new createjs.SpriteSheet({images: ["Kamie Special.png"], frames: [[0,0,38,149,0,-14.25,122.1],[38,0,110,216,0,-16.25,146.1],[0,216,179,273,0,-16.25,174.1],[0,489,244,319,0,-16.25,198.1]]});
+var Sound_Wave_p = Sound_Wave.prototype = new createjs.Sprite();
+Sound_Wave_p.Sprite_initialize = Sound_Wave_p.initialize;
+Sound_Wave_p.initialize = function() {
+	this.Sprite_initialize(Sound_Wave._SpriteSheet);
+	this.paused = false;
+}
+window.Sound_Wave = Sound_Wave;
+}(window));
+
