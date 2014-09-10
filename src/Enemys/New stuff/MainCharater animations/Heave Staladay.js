a2fa980973d35d28fbe8dcf9aa0b470a1414eeab
Wardell James <wjames@student.neumont.edu>
2014-09-10 05:02:08 +0000

    Animations for specials complete
    git-svn-id: https://svn.neumont.edu:8443/svn/the_epic_game_of_randomness/trunk@121 8e5f334d-e8ea-6740-b88d-dc217f5a1e1b

--- src/Enemys/New stuff/MainCharater animations/Heave Staladay.js
+++ src/Enemys/New stuff/MainCharater animations/Heave Staladay.js
@@ -2,7 +2,7 @@
 Heave_Staliday = function() {
 	this.initialize();
 }
-Heave_Staliday._SpriteSheet = new createjs.SpriteSheet({images: ["Heave Staladay.png"], frames: [[0,0,200,300,0,103.5,294.05],[0,300,200,300,0,90.5,299.05],[0,600,200,300,0,88.5,306.05]]});
+Heave_Staliday._SpriteSheet = new createjs.SpriteSheet({images: ["Heave Staladay.png"], frames: [[0,0,200,300,0,103.5,294.05],[200,0,200,300,0,90.5,299.05],[0,300,200,300,0,88.5,306.05],[200,300,283,300,0,90.5,299.05]]});
 var Heave_Staliday_p = Heave_Staliday.prototype = new createjs.Sprite();
 Heave_Staliday_p.Sprite_initialize = Heave_Staliday_p.initialize;
 Heave_Staliday_p.initialize = function() {
--- src/Enemys/New stuff/MainCharater animations/Kamie Jing.js
+++ src/Enemys/New stuff/MainCharater animations/Kamie Jing.js
@@ -2,7 +2,7 @@
 Kamie_Jing = function() {
 	this.initialize();
 }
-Kamie_Jing._SpriteSheet = new createjs.SpriteSheet({images: ["Kamie Jing.png"], frames: [[0,0,200,300,0,107,296.95],[0,300,200,300,0,90,300.95],[0,600,200,300,0,99,291.95]]});
+Kamie_Jing._SpriteSheet = new createjs.SpriteSheet({images: ["Kamie Jing.png"], frames: [[0,0,200,300,0,107,296.95],[200,0,200,300,0,90,300.95],[0,300,200,300,0,99,291.95],[200,300,200,300,0,42,291.95]]});
 var Kamie_Jing_p = Kamie_Jing.prototype = new createjs.Sprite();
 Kamie_Jing_p.Sprite_initialize = Kamie_Jing_p.initialize;
 Kamie_Jing_p.initialize = function() {
